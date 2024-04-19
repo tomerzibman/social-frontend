54afb9d280df77230de4f1de1205fa2c390d6432
tomerzibman <tomerzibman@gmail.com>
2024-04-19 01:31:17 -0400

    added client side validation to signup page

--- src/components/SignUpPage.jsx
+++ src/components/SignUpPage.jsx
@@ -18,9 +18,84 @@ const SignUpPage = ({ handleSignUp }) => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [photo, setPhoto] = useState(null);
   const [preview, setPreview] = useState(null);
+  const [usernameError, setUsernameError] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
 
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
+  const checkUsername = (text) => {
+    if (!text) {
+      setUsernameError("username is required");
+      return false;
+    } else if (!/^[a-zA-Z0-9]+$/.test(text)) {
+      setUsernameError("username consists only of letters and numbers");
+      return false;
+    }
+    setUsernameError("");
+    return true;
+  };
+
+  const handleUsernameChange = ({ target }) => {
+    setUsername(target.value);
+    checkUsername(target.value);
+  };
+
+  const checkName = (text) => {
+    if (!text) {
+      setNameError("name is required");
+      return false;
+    } else if (!/[a-zA-Z0-9]+( [a-zA-Z0-9]+)*$/.test(text)) {
+      setNameError("name consists only of letters, numbers, and spaces");
+      return false;
+    } else {
+      setNameError("");
+      return true;
+    }
+  };
+
+  const handleNameChange = ({ target }) => {
+    setName(target.value);
+    checkName(target.value);
+  };
+
+  const checkPassword = (text) => {
+    if (!text) {
+      setPasswordError("password is required");
+      return false;
+    } else if (text.length < 5) {
+      setPasswordError("password must be at least 5 characters");
+      return false;
+    } else {
+      setPasswordError("");
+      return true;
+    }
+  };
+
+  const handlePasswordChange = ({ target }) => {
+    setPassword(target.value);
+    checkPassword(target.value);
+  };
+
+  const checkConfirmPassword = (text) => {
+    if (text !== password) {
+      setConfirmPasswordError("passwords must match");
+      return false;
+    } else if (!text) {
+      setConfirmPasswordError("confirm password required");
+      return false;
+    } else {
+      setConfirmPasswordError("");
+      return true;
+    }
+  };
+
+  const handleConfirmPasswordChange = ({ target }) => {
+    setConfirmPassword(target.value);
+    checkConfirmPassword(target.value);
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
 
     if (file) {
       const reader = new FileReader();
@@ -36,11 +111,15 @@ const SignUpPage = ({ handleSignUp }) => {
 
   const doSignUp = async (event) => {
     event.preventDefault();
-    // const form = event.currentTarget;
-    // if (form.checkValidity() === false) {
-    //   setValidated(true);
-    //   return;
-    // }
+    const valid =
+      checkUsername(username) &
+      checkName(name) &
+      checkPassword(password) &
+      checkConfirmPassword(confirmPassword);
+    if (!valid) {
+      console.log("we have errors");
+      return;
+    }
 
     const userFormData = new FormData();
     userFormData.append("username", username);
@@ -67,15 +146,17 @@ const SignUpPage = ({ handleSignUp }) => {
           <Typography variant="h5" component="h2" align="center" gutterBottom>
             Signup
           </Typography>
-          <form onSubmit={doSignUp} encType="multipart/form-data">
+          <form onSubmit={doSignUp} encType="multipart/form-data" noValidate>
             <TextField
               variant="outlined"
               margin="normal"
               fullWidth
               label="Username"
               value={username}
-              onChange={(e) => setUsername(e.target.value)}
+              onChange={handleUsernameChange}
               required
+              error={usernameError !== ""}
+              helperText={usernameError}
             />
             <TextField
               variant="outlined"
@@ -83,8 +164,10 @@ const SignUpPage = ({ handleSignUp }) => {
               fullWidth
               label="Name"
               value={name}
-              onChange={(e) => setName(e.target.value)}
+              onChange={handleNameChange}
               required
+              error={nameError !== ""}
+              helperText={nameError}
             />
             <TextField
               variant="outlined"
@@ -93,8 +176,10 @@ const SignUpPage = ({ handleSignUp }) => {
               label="Password"
               type="password"
               value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={handlePasswordChange}
               required
+              error={passwordError !== ""}
+              helperText={passwordError}
             />
             <TextField
               variant="outlined"
@@ -103,8 +188,10 @@ const SignUpPage = ({ handleSignUp }) => {
               label="Confirm Password"
               type="password"
               value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
+              onChange={handleConfirmPasswordChange}
               required
+              error={confirmPasswordError !== ""}
+              helperText={confirmPasswordError}
             />
             <input
               accept="image/*"
